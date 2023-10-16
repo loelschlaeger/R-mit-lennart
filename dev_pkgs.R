9389033c8254a9caaace9955b7fbae0cb189f1c4
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-10-16 18:54:24 +0200

    Initial commit

---
+++ dev_pkgs.R
@@ -0,0 +1,11 @@
+if (require("devtools")) {
+  install.packages("devtools")
+}
+
+if (require("usethis")) {
+  install.packages("usethis")
+}
+
+if (require("yaml")) {
+  install.packages("yaml")
+}
